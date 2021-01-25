7c5f6efa198ac27ecd27cd572a88bc54ddcb8db0
William elTurk <williamelturk@gmail.com>
2021-01-25 12:36:33 +0200

    fix the update simple pages

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,13 +1,14 @@
 const User = require('../models/user')
 
 exports.userAdd = (req,res) => {
-  const {username} = req.body
+  const {username} = req.body[0]
+  console.log(username)
   User.findOne({username}).exec((err,user) => {
     if(user) {
         return res.status(400).json({error: 'Username is taken'})
     }
   })
-  let newUser = new User(req.body);
+  let newUser = new User(req.body[0]);
   newUser.save((err,success) => {
     if (err) {
       return res.status(400).json({
