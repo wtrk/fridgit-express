edf4caae6ca39d5e2bd1e45213c3b2f330271aa6
William elTurk <williamelturk@gmail.com>
2021-01-26 16:20:00 +0200

    search and simple pages

--- controllers/userTypesControllers.js
+++ controllers/userTypesControllers.js
@@ -2,7 +2,6 @@ const UserType = require('../models/userTypes')
 
 
 exports.userTypeAdd = async (req,res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
   console.log("req","ppp",req.body)
   let newUserType = await UserType.insertMany(req.body, function(err, userTypes) {
     if (err) {
@@ -13,17 +12,17 @@ exports.userTypeAdd = async (req,res) => {
       return res.json("Successfully added");
   })
 }
-exports.userTypesList = async (req,response) => {
+exports.userTypesList = async (req,res) => {
   let userTypes = await UserType.find({}, function(err, userTypes) {
     if (err) {
-      return response.status(400).json({
+      return res.status(400).json({
         error: err,
       });
     }
       if(userTypes.length===0) {
-          return response.status(400).json({error: 'No user types in the database'})
+          return res.status(400).json({error: 'No user types in the database'})
       }
-      return response.json(userTypes);
+      return res.json(userTypes);
     
   });
 }
